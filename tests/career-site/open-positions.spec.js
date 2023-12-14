a52e4564b8ece3ce20dba890d268e1552b107e13
Tuyet Doan <tuyet@mgisolutions.com>
2023-12-14 15:12:32 +0700

    Career site - open position

--- tests/career-site/open-positions.spec.js
+++ tests/career-site/open-positions.spec.js
@@ -12,7 +12,7 @@ test.describe('open positions', () => {
     })
     test('have header and footer', async ({page}) => {
         await expect(page.locator('header')).toBeAttached();
-        await expect(page.locator('header')).toBeVisible();
+        await expect(page.locator('header nav')).toBeVisible();
         await expect(page.locator('footer')).toBeAttached();
         await expect(page.locator('footer')).toBeVisible();
     })
@@ -33,11 +33,30 @@ test.describe('open positions', () => {
             await expect((await keyEle.innerText()).toLowerCase()).toBe(keyName);
             await keyEle.click();
             for(let i = 0; i < attrs[key].length; i++){
-                await expect(await (await page.$$(`.dropdown-list-${keyName} li:nth-child(${i+1}) .label-filter`))[0].innerText()).toBe(attrs[key][i].name);
-                await expect(+(await (await page.$$(`.dropdown-list-${keyName} li:nth-child(${i+1}) input`))[0].inputValue())).toBe(attrs[key][i].id);
+                let ele = await page.locator(`.dropdown-list-${keyName} li:nth-child(${i+1})`);
+                await expect(await (await ele.locator(`.label-filter`)).innerText()).toBe(attrs[key][i].name);
+                await expect(+(await (await ele.locator(`input`)).inputValue())).toBe(attrs[key][i].id);
+                await ele.locator(`.check`).click();
+                let jobAfterFilter =await Promise.all(
+                    (await page.$$('.job-listing-component a')).map(async c => await c.getAttribute('href'))
+                ) 
+                await expect(jobAfterFilter).toEqual(jobs.filter(c => c[`${keyName}Id`] == attrs[key][i].id).map(c => `/open-positions/${c.slug}`));
+                await ele.locator(`.check`).click();
             }
         }
 
+        let jobElems = await page.$$('.job-listing-component > a');
+        await expect(jobs?.length).toEqual(jobElems?.length);
+        for(let i = 0; i < jobElems?.length; i++){
+            let ele = page.locator(`.job-listing-component a:nth-child(${i+1})`);
+            await expect((await ele.locator(`h3`).innerText()).trim()).toEqual(jobs[i].title?.trim())
+            await expect((await ele.locator(`.__content__left__position`).innerText()).trim()).toEqual(jobs[i].summary?.trim())
+            await ele.click({position: { x: 23, y: 32 }});
+            await page.waitForLoadState();
+            await expect(page).toHaveURL(`${process.env.CAREER_URL}/open-positions/${jobs[i].slug}`);
+            await page.goBack();
+            await page.waitForLoadState();
+        }
     })
 })
 
